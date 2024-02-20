016ed5d57ce97bcd080810f9f9fb057eeb27ead8
Eduardo Borges <ti22mau@gmail.com>
2024-02-20 19:39:17 -0300

    Commit de ajustes no header

--- src/components/SearchComponent/SearchComponent.jsx
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -1,20 +1,18 @@
-import { MagnifyingGlass } from "phosphor-react";
-import {
-  FormComponent,
-  InputSearch,
-} from "./SearchComponentStyle";
+import { Search } from "@mui/icons-material";
+import { Box, TextField } from "@mui/material";
 
 export function SearchComponent() {
   return (
-    <FormComponent action="">
-      <div>
-        <label htmlFor="inputsearch">Digite o nome</label>
-        <InputSearch
-          id="inputsearch"
-          type="text"
-        />
-        <MagnifyingGlass className="searchIcon" size={30} />
-      </div>
-    </FormComponent>
+    <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '20vw' }}>
+      <Search sx={{ color: '#fff', mr: 1, my: 0.5, fontSize: 30 }} />
+      <TextField fullWidth label="Buscar" variant="standard"
+        InputLabelProps={{
+          style: { fontSize: '1.6rem', color: '#fff' },
+        }}
+        inputProps={{
+          style: { fontSize: '1.6rem', color: '#fff' },
+        }}
+      />
+    </Box>
   );
 }
